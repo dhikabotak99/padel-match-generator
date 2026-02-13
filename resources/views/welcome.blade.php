ed22e72386911d016d56dfc1fd60e27994869282
Andhika Wicaksono <wicakbotak99@gmail.com>
2026-02-14 03:16:08 +0700

    feat: add initial PadelIn home page, branding assets, and frontend entry point.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Padel Match Generator</title>
+    <title>PadelIn</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="antialiased">
